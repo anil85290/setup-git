b0c81f5999ed37fe3a68920cfedd2c6dec7454a7
anil85290 <145982630+anil85290@users.noreply.github.com>
2023-09-27 14:07:24 +0530

    changed background color to green
    green

--- setup.js
+++ setup.js
@@ -1,4 +1,17 @@
-console.log("starting test server")
-console.dir(document)
-console.log(document.title)
-console.log(document.)
+// console.log("starting test server");
+// console.dir(document);
+// console.log(document.title);
+// console.log(document.forms);
+// console.log(document.links);
+//console.log(document.getElementById('header-title'));
+var header=document.getElementById('header-title');
+console.log(header)
+header.textContent='hello';
+header.innerText='1234';
+header.style.borderBottom='solid 3px #000'
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+items[1].textContent='hello2';
+for(let i=0; i<items.length; i++){
+    items[i].style.backgroundColor='green';
+}
